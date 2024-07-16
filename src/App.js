123dc565784978e8b908004db54fb42a84138fa8
Yeonjuu35 <yeonjuu35@gmail.com>
2024-07-16 18:20:22 +0900

    헤더 컴포넌트 작업 시작

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import Header from './component/nav/Header'
+import MainSwiper from './component/MainSwiper'
+import MainContents from './component/MainContents'
+import Issue from './component/Issue'
+import Campaign from './component/Campaign'
+import MainProject from './component/MainProject'
+import Support from './component/Support'
+import Newslatter from './component/Newslatter'
+import Footer from './component/nav/Footer'
+import QuickButton from './component/QuickButton'
 
 function App() {
   return (
-    <></>
+    <>
+      <Header></Header>
+      <MainSwiper></MainSwiper>
+      <MainContents></MainContents>
+      <Issue></Issue>
+      <Campaign></Campaign>
+      <MainProject></MainProject>
+      <Support></Support>
+      <Newslatter></Newslatter>
+      <Footer></Footer>
+      <QuickButton></QuickButton>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import App from './App';
 // 플러그인
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-// import './styles.scss';
-// import 'swiper/swiper-bundle.min.css';
+
+// CSS
+import '../src/scss/common.scss';
+import '../src/scss/nav.scss'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
